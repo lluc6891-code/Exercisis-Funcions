25ef1118623a0ffbc4220dfbcf67a6ced55f78a4
Lluc <lluc6891@iessineu.net>
2026-04-23 10:13:58 +0200

    finished one activity

--- Exercici_10.java
+++ Exercici_10.java
@@ -8,19 +8,14 @@ public class Exercici_10 {
         Scanner factorialScanner = new Scanner(System.in);
         System.out.println("Introdueix un nombre per coneixer el seu factorial:");
         int factorial = factorialScanner.nextInt();
-        int resultat = 1;
-        for (int i = 1; i <= factorial; i++) {
-            resultat = resultat * i;
-        }
-        // Fa les multiplicacions una passa cada vegada, fins que arriba al factorial.
-        System.out.println("El factorial de " + factorial + " és: " + factorial(factorial));
+        System.out.println("El factorial de " + factorial + " es: " + factorial(factorial));
         factorialScanner.close();
 
     }
 
     public static double factorial(int factorial) {
         int resultat = 1;
-        for (int i = 1; i <= factorial; i++) {
+        for (int i = 1; i <= factorial; i++) { // Fa les multiplicacions una passa cada vegada, fins que arriba al factorial.
             resultat = resultat * i;
         }
         return resultat;
