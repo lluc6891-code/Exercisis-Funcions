8f4539229b8105c64b9a91a0ff23cf9b58d525c8
Lluc <lluc6891@iessineu.net>
2026-04-29 09:10:56 +0200

    Finished 15, all done (theoretically)

--- Exercici_15.java
+++ Exercici_15.java
@@ -1,30 +1,39 @@
+
 import java.util.Scanner;
 
 public class Exercici_15 {
 
-    @SuppressWarnings("ConvertToTryWithResources")
-    public static void main(String[] args) {
-        Scanner fraseScanner = new Scanner(System.in);
-        System.out.println("Introdueix una frase per veure si és un pangrama:");
-        String frase = fraseScanner.nextLine().toUpperCase();
-        int[] llista = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        // String Lletres = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+    public static int verificacioPangrama(int correcte, String frase, int[] llista) {
         for (int i = 0; i < frase.length(); i++) {
             char caracter = frase.charAt(i);
             int posicio = (int) caracter - 65;
             llista[posicio] = 1;
         }
-        boolean correcte = true;
         for (int i = 0; i < llista.length; i++) {
             if (llista[i] == 0) {
-                correcte = false;
+                correcte = 0;
             }
         }
-        if (correcte) {
-            System.out.println("Error! No conté tots els caràcters!");
-        } else {
-            System.out.println("La frase introduida és un panagrama!");
-        }
+        return correcte;
+    }
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        int correcte = 1;
+        Scanner fraseScanner = new Scanner(System.in);
+        System.out.println("Introdueix una frase per veure si és un pangrama:");
+        String frase = fraseScanner.nextLine().toUpperCase();
+        int[] llista = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        // String Lletres = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+
         fraseScanner.close();
+        switch (verificacioPangrama(correcte, frase, llista)) {
+            case 1:
+                System.out.println("La frase introduida és un panagrama!");
+                break;
+            default:
+                System.out.println("Error! No conté tots els caràcters!");
+                break;
+        }
     }
 }
